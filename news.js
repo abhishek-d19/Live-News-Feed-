3df83fd7b4f86feea838466031e68c275435af0b
Abhishek deshmukh <abhishek19072006@gmail.com>
2026-06-18 20:51:11 +0530

    Update news.js

--- news.js
+++ news.js
@@ -7,3 +7,26 @@ const loading = document.getElementById("loading");
 
 // API key for fetching live news
 const API_KEY = "e831552f3144c501b0d8ee8e43508986";
+
+
+
+//ad
+
+/*
+Fetch news articles from GNews API based on
+search keyword or selected category.
+*/
+async function getNews(query = "technology") {
+    try {
+        // Display loading message while data is being fetched
+        loading.innerText = "Loading news...";
+
+        const response = await fetch(
+            `https://gnews.io/api/v4/search?q=${query}&lang=en&apikey=${API_KEY}`
+        );
+
+        // Convert JSON response into JavaScript object
+        const data = await response.json();
+
+        // Send articles to display function
+        displayNews(data.articles);
